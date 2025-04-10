c4e5dc017e2e491828bb2b167d8dd714ac010774
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-10 12:58:55 +0200

    Fixes

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,9 +4,9 @@ import "./styles.css";
 export default async function HomePage() {
   return (
     <div className="grid min-h-svh items-center justify-items-center">
-      <div className="container text-center">
+      <div className="container space-y-2 text-center">
         <h1 className="text-5xl">Wetlands!</h1>
-        <p className="h-5 w-5 text-sm">This is the frontend application.</p>
+        <p className="text-sm">This is the frontend application.</p>
         <div className="grid">
           <Link href="/admin" className="card">
             Admin &rarr;
