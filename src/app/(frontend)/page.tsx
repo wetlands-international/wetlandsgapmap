4134d2cc5775c3f5dd3c77e27d0c65d5cd402f41
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-10 12:50:23 +0200

    Eslint, prettier and editorconfig

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,16 +1,18 @@
-import './styles.css'
+import Link from "next/link";
+import "./styles.css";
 
 export default async function HomePage() {
   return (
-    <div className="container">
-      <h1 className="text-4xl">Welcome to the Frontend!</h1>
-      <p className="text-sm">This is the frontend application.</p>
-      <div className="grid">
-        <a href="/admin" className="card">
-          <h2>Admin &rarr;</h2>
-          <p>Explore the Admin</p>
-        </a>
+    <div className="grid min-h-svh items-center justify-items-center">
+      <div className="container text-center">
+        <h1 className="text-5xl">Wetlands!</h1>
+        <p className="h-5 w-5 text-sm">This is the frontend application.</p>
+        <div className="grid">
+          <Link href="/admin" className="card">
+            Admin &rarr;
+          </Link>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,17 +1,17 @@
 // storage-adapter-import-placeholder
-import { postgresAdapter } from '@payloadcms/db-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import { fileURLToPath } from 'url'
-import sharp from 'sharp'
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import { fileURLToPath } from "url";
+import sharp from "sharp";
 
-import { Users } from './collections/Users'
-import { Media } from './collections/Media'
+import { Users } from "./collections/Users";
+import { Media } from "./collections/Media";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
   admin: {
@@ -22,13 +22,13 @@ export default buildConfig({
   },
   collections: [Users, Media],
   editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
+  secret: process.env.PAYLOAD_SECRET || "",
   typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
+    outputFile: path.resolve(dirname, "payload-types.ts"),
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.DATABASE_URI || "",
     },
   }),
   sharp,
@@ -36,4 +36,4 @@ export default buildConfig({
     payloadCloudPlugin(),
     // storage-adapter-placeholder
   ],
-})
+});
