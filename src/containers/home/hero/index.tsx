8b3948f7bedb64601a062969d62107ce3d35bbbd
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-15 11:15:42 +0200

    Shadcn: init and components

--- src/containers/home/hero/index.tsx
+++ src/containers/home/hero/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
 import Link from "next/link";
 
@@ -11,9 +12,9 @@ export const Hero = () => {
       <div className="container space-y-2 text-center">
         <h1 className="text-5xl">{t("home-hero-title")}</h1>
         <p className="text-sm">{t("home-hero-description")}</p>
-        <div className="grid">
+        <Button size="lg" variant="link">
           <Link href="/admin">Admin &rarr;</Link>
-        </div>
+        </Button>
       </div>
     </div>
   );
