d64526f3f79d299f9338ee33a4de8b6c214117a6
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-04-28 10:51:40 +0200

    Playground: points

--- src/containers/playground/index.tsx
+++ src/containers/playground/index.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+
 export const Playground = () => {
   return (
-    <div className="grid min-h-[calc(100svh_-_theme(spacing.16))] items-center justify-items-center"></div>
+    <div className="grid min-h-[calc(100svh_-_theme(spacing.16))] items-center justify-items-center">
+      <Canvas linear>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[1, 2, 5]} />
+
+        <points>
+          <torusKnotGeometry args={[1, 0.4, 100, 16]} />
+          <pointsMaterial size={0.05} color="orange" />
+        </points>
+
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 };
