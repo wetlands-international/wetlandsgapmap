6555c6f2c55e25e926cd8eb189e31957309c283d
Alejandro Peralta <alepefe@protonmail.com>
2025-05-15 17:47:49 +0200

    chore: Infrastructure test

--- titiler/main.py
+++ titiler/main.py
@@ -1,11 +1,17 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from titiler.core.factory import TilerFactory
 
-app = FastAPI(title="Mini Titiler")
+load_dotenv()
+
+TITILER_BASE_URL = os.getenv("TITILER_BASE_URL", "/tiler")
+
+app = FastAPI(title="Mini Titiler", root_path=TITILER_BASE_URL)
 
 # Create a tiler (COG = Cloud-Optimized GeoTIFF)
 cog_tiler = TilerFactory()
-app.include_router(cog_tiler.router, prefix="/cog", tags=["cog"])
+app.include_router(cog_tiler.router, tags=["tiles"])
 
 @app.get("/healthz")
 def health():
